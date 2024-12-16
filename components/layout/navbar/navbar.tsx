194a5407ddfea761a6c57f1fce6856f8b1cbf2a5
nxhettry <nishan.gtsn@gmail.com>
2024-12-16 20:44:06 +0545

    creating a navbar

---
+++ components/layout/navbar/navbar.tsx
@@ -0,0 +1,23 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+
+export default function Navbar() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>School</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
---
+++ components/layout/navbar/navlinks.ts
@@ -0,0 +1,9 @@
+export const Navlinks = [
+  {
+    heading: "School",
+    subheading: [
+      { name: "View All", link: "/schools" },
+      { name: "Add New", link: "/schools/addnew" },
+    ],
+  },
+];
