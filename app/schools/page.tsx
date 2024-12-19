7ced8e9f585e7410d263331375aa2b37c0e0e93a
nxhettry <nishan.gtsn@gmail.com>
2024-12-19 23:30:03 +0545

    handling multiple data

--- app/schools/page.tsx
+++ app/schools/page.tsx
@@ -30,8 +30,14 @@ export default async function Schools() {
   }
 
   return (
-    <div className="h-full w-full flex justify-center items-center text-white text-3xl">
-      <DataTableDemo data={data.message[0].data} />
+    <div className="h-full w-4/5 flex flex-col overflow-y-scroll justify-center items-center space-y-8 mx-auto rounded-xl p-3 text-black">
+      {data.message.map((item, index) => {
+        return (
+          <div key={index} className="bg-gray-50 rounded-xl p-3 w-full">
+            <DataTableDemo data={item.data} />
+          </div>
+        );
+      })}
     </div>
   );
 }
