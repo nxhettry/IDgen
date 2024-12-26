5d25d44a47fd96f71f1bc8e169d7a1d6df16f8fc
nxhettry <nishan.gtsn@gmail.com>
2024-12-26 09:50:19 +0545

    finally cleared the mess

--- app/api/school/[schoolid]/route.ts
+++ app/api/school/[schoolid]/route.ts
@@ -1,13 +1,48 @@
 import { NextResponse } from "next/server";
-// import { connectDB } from "@/lib/db";
-// import Data from "@/models/DataSchema";
+import { connectDB } from "@/lib/db";
+import Data from "@/models/DataSchema";
 
 export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
+  const schoolIndex = parseInt(req.url.split("/")[5]);
+  const _id = req.headers.get("X-userId");
 
-  const schoolIndex = searchParams.get("schoolid");
+  if (!_id) {
+    console.log("_id not received in the dynamic api route");
+    return NextResponse.json({
+      status: 404,
+      message: "An error occured. Please Try again later",
+    });
+  }
 
-  console.log("Received index : ", schoolIndex);
+  try {
+    await connectDB();
+    const allSchools = await Data.find({ userId: _id });
 
-  return NextResponse.json({ message: "Hello" });
+    if (allSchools.length === 0) {
+      return NextResponse.json({
+        status: 404,
+        message: "No Data found",
+      });
+    }
+
+    const school = allSchools[schoolIndex];
+
+    if (!school) {
+      return NextResponse.json({
+        status: 404,
+        message: "No Data found",
+      });
+    }
+
+    return NextResponse.json({
+      status: 200,
+      data: school.data,
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({
+      status: 500,
+      message: "Internal Server Error",
+    });
+  }
 }
--- app/schools/[schoolid]/page.tsx
+++ app/schools/[schoolid]/page.tsx
@@ -1,10 +1,30 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/lib/authOptions";
 import axios from "axios";
 
 const Class = async ({ params }: { params: { schoolid: number } }) => {
   const { schoolid } = await params;
 
+  // Getting the session details
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return (
+      <div className="mt-24 flex justify-center items-center">
+        Please Login to continue
+      </div>
+    );
+  }
+
   try {
-    const res = await axios.get(`${process.env.NEXTAUTH_URL}/api/school/${schoolid}`);
+    const res = await axios.get(
+      `${process.env.NEXTAUTH_URL}/api/school/${schoolid}`,
+      {
+        headers: {
+          "X-userId": session.user._id,
+        },
+      }
+    );
 
     console.log(res.data);
   } catch (error) {
